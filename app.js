af2a8aade6fc2be4450706229e0626a2e09bf86f
Arunkumar <I3518@IND-CHN-MAC1217.local>
2024-11-10 14:12:33 +0530

    comments removed

--- app.js
+++ app.js
@@ -24,13 +24,6 @@ app.use(express.json());
 app.use("/auth", userRouter);
 app.use("/borrower", loanRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
