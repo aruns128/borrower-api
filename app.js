93fa97be7dcef9f026edd1e30350fa1f5c0d35ac
Arunkumar <I3518@IND-CHN-MAC1217.local>
2024-11-10 11:11:03 +0530

    user model and auth setup added

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
 require("dotenv").config();
 require("./database/connection");
 
-const clientUrl = process.env.DEV_REACT_URL;
 const app = express();
 
 //Middleware
 app.use(cookieParser());
 app.use(cors());
 
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(morgan("dev"));
 app.use(express.json());
+
+//Routes
+// app.use(userRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
